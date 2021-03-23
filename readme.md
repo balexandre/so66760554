956fb988286d2519682dc172c1ee4cbcbdd51d0c
Bruno Alexandre <bruno.in.dk@gmail.com>
2021-03-23 19:36:14 +0100

    initial commit

---
+++ readme.md
@@ -0,0 +1,21 @@
+# StackOverflow helper for question 66760554
+
+[Original question](https://stackoverflow.com/q/66760554/28004)
+
+## to run
+
+change both SendGrid API Key and sender email in the code
+
+```javascript
+const SENDGRID_API_KEY = 'SENDGRID_API_KEY'
+const SENDGRID_SENDER_EMAIL = 'SENDGRID_SENDER_EMAIL'
+```
+
+install and start server with
+
+```bash
+npm i
+npm start
+```
+
+if using [VSCode REST Client](https://marketplace.visualstudio.com/items?itemName=humao.rest-client) plugin, open `_.http` and run the request
---
+++ server/index.js
@@ -0,0 +1,37 @@
+const crypto = require("crypto")
+const express = require('express')
+const sgMail = require('@sendgrid/mail')
+const asyncHandler = require("express-async-handler")
+
+const SENDGRID_API_KEY = 'SENDGRID_API_KEY'
+const SENDGRID_SENDER_EMAIL = 'SENDGRID_SENDER_EMAIL'
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY || SENDGRID_API_KEY)
+
+app.get('/password/reset/:token', asyncHandler((req, res) => {
+    res.json({ token: req.params.token })
+}))
+app.post('/forgot', asyncHandler(async (req, res) => {
+    const user = {
+        token: crypto.randomBytes(20).toString('hex'),
+        email: 'destination_email'
+    }
+    const resetUrl = `http://${req.headers.host}/password/reset/${user.token}`
+
+    const msg = {
+        to: user.email,
+        from: SENDGRID_SENDER_EMAIL,
+        subject: "PASSWORD RESET LINK",
+        html: `
+            <p><a href="${resetUrl}">click to reset your password</a></p>
+        `,
+    }
+
+    await sgMail.send(msg)
+    res.json({ message: 'email sent' })
+}))
+
+app.listen(PORT, () => console.log(`open http://localhost:${PORT}`))
